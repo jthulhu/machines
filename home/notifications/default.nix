2cbc0d2574f08e29128581ba5b7bc24cd6596cf9
BlackBeans <adrien.lc.mathieu@gmail.com>
2022-01-25 20:38:05 +0100

    [home] mako -> notifioncations, replacing mako with dunst.

---
+++ home/notifications/default.nix
@@ -0,0 +1,14 @@
+{
+  # programs.mako = {
+  #   enable = true;
+  #   anchor = "top-center";
+  #   borderRadius = 2;
+  #   font = "monospace 15";
+  #   extraConfig = ''
+  #   text-alignment=center
+  #   '';
+  # };
+  services.dunst = {
+    enable = true;
+  };
+}
