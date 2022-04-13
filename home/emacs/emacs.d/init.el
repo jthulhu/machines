1cad3e38361ed9efa05e26f53f8c5c2b0c000bf6
BlackBeans <adrien.lc.mathieu@gmail.com>
2022-04-13 11:05:41 +0200

    [home/emacs] Create automatically directories when opening file.

--- home/emacs/emacs.d/init.el
+++ home/emacs/emacs.d/init.el
@@ -71,6 +71,13 @@
       (find-file (read-file-name "Find File: "))
     (crux-sudo-edit)))
 
+(defadvice find-file (before make-directory-maybe (filename &optional wildcards) activate)
+  "Create parent directory if not exists while visiting file."
+  (unless (file-exists-p filename)
+    (let ((dir (file-name-directory filename)))
+      (unless (file-exists-p dir)
+        (make-directory dir t)))))
+
 (defun dashboard-focus ()
   (interactive)
   (switch-to-buffer "*dashboard*"))
