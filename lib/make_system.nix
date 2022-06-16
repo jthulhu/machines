54f3bba929716aa650ae6471caf1de9f284279de
BlackBeans <adrien.lc.mathieu@gmail.com>
2022-06-16 20:03:21 +0200

    [flake,lib/make_system] Removed nixpkgs-wayland.

--- lib/make_system.nix
+++ lib/make_system.nix
@@ -19,14 +19,6 @@ let
         };
         flake = nixpkgs;
       };
-      binaryCachePublicKeys = [
-        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
-        "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
-      ];
-      binaryCaches = [
-        "https://cache.nixos.org"
-        "https://nixpkgs-wayland.cachix.org"
-      ];
     };
   };
   common = "${self}/system/configuration.nix";
