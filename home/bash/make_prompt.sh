22412a21d75f7541a1bc9326f3a6eb4a23e557a9
BlackBeans <adrien.lc.mathieu@gmail.com>
2022-07-04 12:30:29 +0200

    [home/bash/make_prompt] Added direnv support.

--- home/bash/make_prompt.sh
+++ home/bash/make_prompt.sh
@@ -6,7 +6,8 @@ USER_COLOR='\[\e[01;32m\]'
 ROOT_COLOR='\[\e[01;31m\]'
 RESET_COLOR='\[\e[0m\]'
 
-PS_GIT='$(GIT_TEXT=`isgit -c -b status`; echo -en "${GIT_TEXT:+ (\[`isgit -c -b pre`\]$GIT_TEXT\[`isgit -c -b post`\])}")'
+PS_GIT='$(GIT_TEXT=`isgit -c -b status`; echo -en "${GIT_TEXT:+ \[\e[36m\](\[\e[0m\]\[`isgit -c -b pre`\]$GIT_TEXT\[`isgit -c -b post`\])}"\[\e[36m\])\[\e[0m\]'
+PS_DIRENV='${DIRENV_PROMPT:+ \[\e[36m\](\[\e[0;1;33m\]$DIRENV_PROMPT\[\e[0;36m\])\[\e[0m\]}'
 PS_DATE='\D{%d-%m-%Y}'
 PS_PATH="${PATH_COLOR}\w${RESET_COLOR}"
 PS_STATUS='$(RETVAL=$?; [[ "$RETVAL" -eq 0 ]] && echo -en "\[\e[37m\]" || echo -en "\[\e[01;31m\]"; echo "$RETVAL\[\e[0m\]")'
@@ -18,6 +19,6 @@ else
 fi
 
 PS_LINE1="${FRAME_COLOR}╭─{$PS_STATUS:$PS_PATH${FRAME_COLOR}}${RESET_COLOR}${PS_GIT}"
-PS_LINE2="${FRAME_COLOR}╰[${RESET_COLOR}${PS_USER}${FRAME_COLOR}] $FRAME_COLOR\$ $RESET_COLOR"
+PS_LINE2="${FRAME_COLOR}╰[${RESET_COLOR}${PS_USER}${FRAME_COLOR}]$PS_DIRENV $FRAME_COLOR\$ $RESET_COLOR"
 
 export PS1="$PS_LINE1\n$PS_LINE2"
