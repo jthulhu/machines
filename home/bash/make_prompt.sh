2e21649ab1fd28b5f86cf35e70a0683f34c7bbf3
BlackBeans <adrien.lc.mathieu@gmail.com>
2022-07-04 15:20:34 +0200

    [home/bash/make_prompt] Improved PS1.

--- home/bash/make_prompt.sh
+++ home/bash/make_prompt.sh
@@ -6,8 +6,9 @@ USER_COLOR='\[\e[01;32m\]'
 ROOT_COLOR='\[\e[01;31m\]'
 RESET_COLOR='\[\e[0m\]'
 
-PS_GIT='$(GIT_TEXT=`isgit -c -b status`; echo -en "${GIT_TEXT:+ \[\e[36m\](\[\e[0m\]\[`isgit -c -b pre`\]$GIT_TEXT\[`isgit -c -b post`\])}"\[\e[36m\])\[\e[0m\]'
+PS_GIT='$(GIT_TEXT=`isgit -c -b status`; echo -en "${GIT_TEXT:+ \[\e[0;36m\](\[\e[0m\]\[`isgit -c -b pre`\]$GIT_TEXT\[`isgit -c -b post`\])}"\[\e[36m\])\[\e[0m\]'
 PS_DIRENV='${DIRENV_PROMPT:+ \[\e[36m\](\[\e[0;1;33m\]$DIRENV_PROMPT\[\e[0;36m\])\[\e[0m\]}'
+PS_NIX_SHELL='${IN_NIX_SHELL:+\[\e[01;31m\]*\[\e[0m\]}'
 PS_DATE='\D{%d-%m-%Y}'
 PS_PATH="${PATH_COLOR}\w${RESET_COLOR}"
 PS_STATUS='$(RETVAL=$?; [[ "$RETVAL" -eq 0 ]] && echo -en "\[\e[37m\]" || echo -en "\[\e[01;31m\]"; echo "$RETVAL\[\e[0m\]")'
@@ -18,7 +19,7 @@ else
     PS_USER="${USER_COLOR}\u${FRAME_COLOR}@${USER_COLOR}\h${RESET_COLOR}"
 fi
 
-PS_LINE1="${FRAME_COLOR}╭─{$PS_STATUS:$PS_PATH${FRAME_COLOR}}${RESET_COLOR}${PS_GIT}"
+PS_LINE1="${FRAME_COLOR}╭─{$PS_STATUS:$PS_PATH${FRAME_COLOR}}${RESET_COLOR}${PS_NIX_SHELL}${PS_GIT}"
 PS_LINE2="${FRAME_COLOR}╰[${RESET_COLOR}${PS_USER}${FRAME_COLOR}]$PS_DIRENV $FRAME_COLOR\$ $RESET_COLOR"
 
 export PS1="$PS_LINE1\n$PS_LINE2"
