1ee3783669065b8e2f5f097fb1242e620179d894
BlackBeans <adrien.lc.mathieu@gmail.com>
2022-08-02 08:50:37 +0200

    [home/xdg] Updated default applications for common mime types.

--- home/xdg/default.nix
+++ home/xdg/default.nix
@@ -6,12 +6,36 @@
   
   xdg = let
     inherit (builtins) listToAttrs;
-    imageFormats = [ "png" "jpeg" "gif" "svg" "tiff" ];
-    imageSupport = apps: listToAttrs (
+
+    broadSupport = cat: formats: apps: listToAttrs (
       map
-        (format: { name = "image/${format}"; value = apps; })
-        imageFormats
+        (format: { name = "${cat}/${format}"; value = apps; })
+        formats
     );
+    imageSupport = broadSupport "image" [ "png" "jpeg" "gif" "svg" "tiff" ];
+    textSupport = broadSupport "text" [
+      "plain"
+      "org"
+      "css"
+      "csv"
+      "html"
+      "javascript"
+      "english"
+      "plain"
+      "x-makefile"
+      "x-c++hdr"
+      "x-c++src"
+      "x-chdr"
+      "x-csrc"
+      "x-java"
+      "x-moc"
+      "x-pascal"
+      "x-tcl"
+      "x-tex"
+      "application/x-shellscript"
+      "x-c"
+      "x-c++"
+    ];
   in {
     enable = true;
     mimeApps = {
@@ -19,11 +43,16 @@
       associations.added = {
         "application/pdf" = [ "org.pwmt.zathura.desktop" "calibre-ebook-viewer.pdf" ];
         "x-scheme-handler/mailto" = [ "thunderbird.desktop" ];
-      } // imageSupport [ "imv.desktop" "gimp.desktop" ];
+        "text/plain" = [ "emacsclient.desktop" ];
+      }
+      // imageSupport [ "imv.desktop" "gimp.desktop" ]
+      // textSupport [ "emacsclient.desktop" "emacs.desktop" ];
       defaultApplications = {
         "application/pdf" = [ "org.pwmt.zathura.desktop" ];
         "x-scheme-handler/mailto" = [ "thunderbird.desktop" ];
-      } // imageSupport [ "imv.desktop" ];
+      }
+      // imageSupport [ "imv.desktop" ]
+      // textSupport [ "emacsclient.desktop" ];
     };
   };
 }
