079f47237fed0c4b8b971fde681246582b83e210
BlackBeans <adrien.lc.mathieu@gmail.com>
2022-08-23 17:28:51 +0200

    [home/{bash,wm}] Made Calibre have the right theme.

--- home/bash/default.nix
+++ home/bash/default.nix
@@ -21,5 +21,8 @@ if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
   exec sway
 fi
 '' + (builtins.readFile ./make_prompt.sh);
+    sessionVariables = {
+      CALIBRE_USE_DARK_PALETTE = 1;
+    };
   };
 }
