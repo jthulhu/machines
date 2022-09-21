11a995a3968102f41e48b6c723ece59636c8a23e
BlackBeans <adrien.lc.mathieu@gmail.com>
2022-09-21 22:52:44 +0200

    [system/layouts] Added russian layout.

--- system/layouts/default.nix
+++ system/layouts/default.nix
@@ -1,9 +1,16 @@
 {
   services.xserver.layout = "bsk";
-  services.xserver.extraLayouts.bsk = {
-    description = "BB Switch";
-    symbolsFile = ./bsk;
-    languages = [ "eng" "fr" "it" ];
+  services.xserver.extraLayouts ={
+    bsk = {
+      description = "BB Switch";
+      symbolsFile = ./bsk;
+      languages = [ "eng" "fr" "it" ];
+    };
+    bru = {
+      description = "BB Russian";
+      symbolsFile = ./bru;
+      languages = [ "ru" ];
+    };
   };
 
   console.useXkbConfig = true;
