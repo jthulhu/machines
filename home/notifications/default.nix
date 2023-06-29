e408542a26398e96e05d756d55c602b897ec4e1f
jthulhu <adrien.lc.mathieu@gmail.com>
2023-06-29 09:24:19 +0200

    [home/notifications] Changed UI.

--- home/notifications/default.nix
+++ home/notifications/default.nix
@@ -1,8 +1,8 @@
 {
-  # programs.mako = {
+  # services.mako = {
   #   enable = true;
-  #   anchor = "top-center";
-  #   borderRadius = 2;
+  #   anchor = "top-right";
+  #   borderRadius = 5;
   #   font = "monospace 15";
   #   extraConfig = ''
   #   text-alignment=center
@@ -10,5 +10,33 @@
   # };
   services.dunst = {
     enable = true;
+    settings = {
+      global = {
+        origin = "top-right";
+        corner_radius = 15;
+        # transparency only works for X11...
+        # transparency = 10;
+        progress_bar_height = 25;
+        progress_bar_frame_width = 0;
+        progress_bar_corner_radius = 5;
+        frame_width = 0;
+        padding = 12;
+        horizontal_padding = 12;
+        highlight= "#808080";
+      };
+      urgency_low = {
+        background = "#222222cc";
+      };
+      urgency_normal = {
+        background = "#222222cc";
+      };
+      urgency_critical = {
+        background = "#332222cc";
+        origin = "top-center";
+        padding = 8;
+        horizontal_padding = 8;
+        frame_width = 10;
+      };
+    };
   };
 }
