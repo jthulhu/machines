f7f62a2bbdae5832903cea66b20e93f7c1b88b54
jthulhu <adrien.lc.mathieu@gmail.com>
2023-07-29 09:47:17 +0200

    [home/fonts] Added font for better symbols.

--- home/fonts/default.nix
+++ home/fonts/default.nix
@@ -2,7 +2,9 @@
 {
   home.packages = with pkgs; [
     font-awesome
-    (nerdfonts.override { fonts = [ "FiraCode" ]; })
+    (nerdfonts.override {
+      fonts = [ "FiraCode" "NerdFontsSymbolsOnly" ];
+    })
     source-code-pro
     xkcd-font
   ];
