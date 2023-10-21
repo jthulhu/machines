449ac5c55e27e71ea49e9f818011a3d79b8fe3b3
jthulhu <adrien.lc.mathieu@gmail.com>
2023-10-21 16:44:55 +0200

    [system/ecryptfs] Apparently, I still need this one...

--- system/ecryptfs/default.nix
+++ system/ecryptfs/default.nix
@@ -1,7 +1,11 @@
 { pkgs, ... }: {
   environment.systemPackages = with pkgs; [
     ecryptfs
-    # ecryptfs-helper
+    ecryptfs-helper
+  ];
+
+  nixpkgs.config.permittedInsecurePackages = [
+    "python-2.7.18.7"           # Thanks, ecryptfs-helper...
   ];
 
   boot.kernelModules = [
