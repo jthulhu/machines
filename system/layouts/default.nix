47f41109dec2fab3662e9ca6765d917e9bad7d7a
jthulhu <adrien.lc.mathieu@gmail.com>
2024-02-28 08:50:52 +0100

    Added support for developping and using a POTITCLAVIER ♥

--- system/layouts/default.nix
+++ system/layouts/default.nix
@@ -1,6 +1,6 @@
-{
+{ pkgs, ... }: {
   services.xserver.layout = "bsk";
-  services.xserver.extraLayouts ={
+  services.xserver.extraLayouts = {
     bsk = {
       description = "BB Switch";
       symbolsFile = ./bsk;
@@ -11,7 +11,21 @@
       symbolsFile = ./bru;
       languages = [ "ru" ];
     };
+    jkb = {
+      description = "Jthulhu's keyboard";
+      symbolsFile = ./jkb;
+      languages = [ "eng" "fr" "it" ];
+    };
   };
+
+  # services.udev.extraRules = ''
+  #   KERNEL=="uinput", SUBSYSTEM=="misc", TAG+="uaccess", OPTIONS+="static_node=uinput", GROUP="input", MODE="0600  '';
+
+  hardware.keyboard.qmk.enable = true;
+  environment.systemPackages = with pkgs; [
+    qmk
+    qmk-udev-rules
+  ];
 
   console.useXkbConfig = true;
 }
--- system/users/adri.nix
+++ system/users/adri.nix
@@ -2,6 +2,6 @@
   isNormalUser = true;
   home = "/home/adri";
   description = "Adrien";
-  extraGroups = [ "wheel" "networkmanager" "libvirtd" "docker" ];
+  extraGroups = [ "wheel" "networkmanager" "libvirtd" "docker" "input" ];
   hashedPassword = "$6$DsjIYMZPRodOWvMt$oiY01fVoOOKaTNNYSCxbX0FjL9BDSElOqcNbq10.vmO4ItjG9jI5iyglfNve8APgM2VNGm05it.M4jyJbFvro/";
 }
