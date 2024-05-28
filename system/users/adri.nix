8aa8f2117584f850d372baa7f832462723916bdf
jthulhu <adrien.lc.mathieu@gmail.com>
2024-05-28 19:30:26 +0200

    [system] Added printer setup.

--- system/users/adri.nix
+++ system/users/adri.nix
@@ -2,6 +2,6 @@
   isNormalUser = true;
   home = "/home/adri";
   description = "Adrien";
-  extraGroups = [ "wheel" "networkmanager" "libvirtd" "docker" "input" ];
+  extraGroups = [ "wheel" "networkmanager" "libvirtd" "docker" "input" "scanner" "lp" ];
   hashedPassword = "$6$DsjIYMZPRodOWvMt$oiY01fVoOOKaTNNYSCxbX0FjL9BDSElOqcNbq10.vmO4ItjG9jI5iyglfNve8APgM2VNGm05it.M4jyJbFvro/";
 }
