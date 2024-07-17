d21af15eea6315db9e468bd866dec463aa33dd6c
jthulhu <adrien.lc.mathieu@gmail.com>
2024-07-17 11:02:09 +0200

    [home] Added locate dabase management for encrypted directories.

--- home/bash/default.nix
+++ home/bash/default.nix
@@ -1,7 +1,9 @@
-{ pkgs, config, ... }:
+{ pkgs, config, lib, ... }:
 let
-  em-directory = "${config.home.homeDirectory}/em";
-  private-locate-db = "${em-directory}/.plocate.db";
+  inherit (lib.strings) concatMapStringsSep;
+  encrypted-places = [ "em" "er" "ed" "series" "movies" ];
+  encrypted-directories = map (dir: "${config.home.homeDirectory}/${dir}") encrypted-places;
+  private-locate-dbs = concatMapStringsSep ":" (dir: "${dir}/.plocate.db") encrypted-directories;
 in {
   home.packages = with pkgs; [
     isgit
@@ -19,17 +21,28 @@ in {
       nano = "emacsclient -nw";
       vim = "emacsclient --nw";
       t = "trash put";
-      udb = "updatedb -l no -o ${private-locate-db} -U ${em-directory}";
     };
     shellOptions = [ "histappend" "checkwinsize" "globstar" ];
     initExtra = ''
+function udb() {
+  echo Updating databases...
+  for dir in ${concatMapStringsSep " " (dir: "\"${dir}\"") encrypted-directories}; do
+    if [ -f "$dir/backup" ]; then
+      echo "Database for $(basename $dir) is accessibe, updating."
+      updatedb -l no -o "$dir/.plocate.db" -U "$dir"
+    else
+      echo "Database for $(basename $dir) is not accessible, skipping."
+    fi
+  done
+}
+
 if [ -z $DISPLAY ] && [ "$(tty)" = "/dev/tty1" ]; then
   exec sway
 fi
 '' + (builtins.readFile ./make_prompt.sh);
     sessionVariables = {
       CALIBRE_USE_DARK_PALETTE = 1;
-      LOCATE_PATH = private-locate-db;
+      LOCATE_PATH = private-locate-dbs;
     };
   };
 }
