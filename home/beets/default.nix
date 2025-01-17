0483241ef937b25be921eb3c6ef9a2ac5e59cebe
jthulhu <adrien.lc.mathieu@gmail.com>
2025-01-17 16:12:48 +0100

    [home/beets] Added config.

--- home/beets/default.nix
+++ home/beets/default.nix
@@ -1,10 +1,25 @@
 { pkgs, ... }:
 {
-  home.packages = with pkgs; [
-    (beets.override {
+  programs.beets = {
+    enable = true;
+    package = pkgs.beets.override {
       pluginOverrides = {
         chroma.enable = true;
+        lyrics.enable = true;
       };
-    })
-  ];
+    };
+    settings = {
+      directory = "~/music";
+      plugins = [ "chroma" "lyrics" ];
+      asciify_paths = true;
+      max_filename_length = 134;
+      "import" = {
+        write = "yes";
+        move = "yes";
+      };
+      paths = {
+        default = "%lower{$albumartist}/%lower{$album}%aunique{}/$track.%lower{$title}";
+      };
+    };
+  };
 }
