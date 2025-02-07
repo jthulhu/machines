9fe6750ea06d4e5bd2996c9d63ea38f947d18c96
jthulhu <adrien.lc.mathieu@gmail.com>
2025-02-07 15:11:13 +0100

    [system/hosts/rlyeh] Added power management options.

--- system/hosts/rlyeh.nix
+++ system/hosts/rlyeh.nix
@@ -6,13 +6,24 @@
     gpu = "intel";
   };
 
-  services.logind = {
-    lidSwitch = "suspend";
-    lidSwitchExternalPower = "lock";
-    powerKey = "ignore";
+  services = {
+    logind = {
+      lidSwitch = "suspend";
+      lidSwitchExternalPower = "lock";
+      powerKey = "ignore";
+    };
+    thermald.enable = true;
+    tlp = {
+      enable = true;
+    };
   };
 
   preset = "personal";
+
+  powerManagement = {
+    enable = true;
+    powertop.enable = true;
+  };
 
   networking.interfaces.enp45s0.useDHCP = true;
   networking.interfaces.wlp0s20f3.useDHCP = true;
