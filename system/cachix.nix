75e8f0a249d8849f6343298a5f1aa734e6268b70
jthulhu <adrien.lc.mathieu@gmail.com>
2025-02-08 18:04:40 +0100

    [system/cachix] Properly setup nix-community binary cache.

--- system/cachix.nix
+++ system/cachix.nix
@@ -1,13 +1,12 @@
-
-# WARN: this file will get overwritten by $ cachix use <name>
-{ pkgs, lib, ... }:
-
-let
-  folder = ./cachix;
-  toImport = name: value: folder + ("/" + name);
-  filterCaches = key: value: value == "regular" && lib.hasSuffix ".nix" key;
-  imports = lib.mapAttrsToList toImport (lib.filterAttrs filterCaches (builtins.readDir folder));
-in {
-  inherit imports;
-  nix.settings.substituters = ["https://cache.nixos.org/"];
+{
+  nix.settings = {
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://nix-community.cachix.org"
+    ];
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 }
