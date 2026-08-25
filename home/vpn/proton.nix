e9cfda96c6ca5522aefc82b65c5d15c952567d99
jthulhu <jthulhu@posteo.net>
2026-08-25 17:25:11 +0200

    [home/vpn]  Added a systemd service for VPN.

--- home/vpn/proton.nix
+++ home/vpn/proton.nix
@@ -3,4 +3,23 @@
   home.packages = with pkgs; [
     proton-vpn-cli
   ];
+  
+  systemd.user.services = {
+    proton-vpn = {
+      Unit = {
+        Description = "Proton VPN";
+        PartOf = "sway-session.target";
+      };
+      Service = {
+        Type = "exec";
+        ExecStart = "${pkgs.proton-vpn-cli}/bin/protonvpn connect";
+        ExecStop = "${pkgs.proton-vpn-cli}/bin/protonvpn disconnect";
+        Restart = "on-failure";
+        RemainAfterExit = true;
+      };
+      Install = {
+        WantedBy = [ "sway-session.target" ];
+      };
+    };
+  };
 }
