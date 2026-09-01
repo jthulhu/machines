5363fad4a64a776ba8ac85d05f50dc299de8fe6f
jthulhu <jthulhu@posteo.net>
2026-09-01 15:48:42 +0200

    [system/vpn]  Moved IRIF VPN to own file.

--- system/vpn/default.nix
+++ system/vpn/default.nix
@@ -1,17 +1,6 @@
-{ pkgs, ... }:
-let
-  inherit (builtins) readFile replaceStrings;
-  inherit (pkgs.irif-vpn) config cert;
-in {
-  services.openvpn = {
-    servers = {
-      irif = {
-        config = replaceStrings ["ca.crt"] [(toString cert)] (readFile config);
-        autoStart = false;
-      };
-    };
-  };
+{
   imports = [
+    ./irif.nix
     ./pia.nix
   ];
   boot.kernelModules = [ "ovpn" ];
---
+++ system/vpn/irif.nix
@@ -0,0 +1,14 @@
+{ pkgs, ... }:
+let
+  inherit (builtins) readFile replaceStrings;
+  inherit (pkgs.irif-vpn) config cert;
+in {
+  services.openvpn = {
+    servers = {
+      irif = {
+        config = replaceStrings ["ca.crt"] [(toString cert)] (readFile config);
+        autoStart = false;
+      };
+    };
+  };
+}
